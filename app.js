be146d7686fd51e213c57b180f8c6e0c5b4d852f
Eric Weijsman <ericweijsman@gmail.com>
2017-12-28 13:34:47 +0100

    Initial commit.

---
+++ app.js
@@ -0,0 +1,26 @@
+var dateformat = require('dateformat');
+var express = require('express');
+app = express();
+
+app.use(express.static('public'));
+console.log('boe');
+app.get('/:time', function(req,res){
+  if(req.params.time == parseInt(req.params.time)){
+    var date = new Date(parseInt(req.params.time)*1000);
+    var naturalDate = dateformat(date, 'mmmm dd, yyyy');
+    res.send({unix:req.params.time, natural:naturalDate});
+    } else {
+      var date = new Date(req.params.time).getTime();
+      if(!isNaN(date)){
+        res.send({unix:date/1000, time:dateformat(req.params.time, 'mmmm dd, yyyy')});
+      } else {
+        res.send({unix:null, time:null});
+      }
+    }
+
+})
+
+
+app.listen(process.env.port||3000, function(){
+  console.log('server running...')
+})
